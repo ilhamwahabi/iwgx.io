72c09c1f414a7a002328f188c5b39e145c7eeeaf
iwgx <wahabiputra@gmail.com>
2020-03-01 01:35:41 +0700

    Update blog meta styling and increase font size

--- src/components/allblogs.tsx
+++ src/components/allblogs.tsx
@@ -33,7 +33,7 @@ export const AllBlogs = () => {
   return (
     <section className="flex-1 py-8 xl:w-2/4 self-center">
       <h2 className="text-3xl mb-4 font-bold mb-2">Blog</h2>
-      {data.allContentfulBlog.edges.map((node, index) => {
+      {data.allContentfulBlog.edges.map(node => {
         const { title, excerpt } = node.node;
 
         const slug = title
@@ -43,12 +43,12 @@ export const AllBlogs = () => {
         const pageUrl = `blog/${slug}`;
 
         return (
-          <div className="bg-white mb-4 py-4 px-6 border-l-4 border-primary shadow-lg" key={index}>
-            <Link className="block" to={pageUrl}>
+          <Link to={pageUrl} key={slug}>
+            <div className="bg-white mb-4 py-4 px-6 border-l-4 border-primary shadow-lg">
               <h2 className="text-2xl font-bold mb-2">{title}</h2>
               <p className="text-base">{excerpt.excerpt}</p>
-            </Link>
-          </div>
+            </div>
+          </Link>
         );
       })}
     </section>
--- src/templates/blog.tsx
+++ src/templates/blog.tsx
@@ -10,22 +10,31 @@ import { renderDate } from '../utils/blog/renderDate';
 import { Header } from '../components/header';
 
 const BlogPost = ({ data }) => {
-  const { title, topics, createdAt, updatedAt, banner, content } = data.contentfulBlog;
+  const { title, topics, createdAt, updatedAt, banner, content, excerpt } = data.contentfulBlog;
 
   return (
     <>
-      <SEO title={title} />
+      <SEO title={title} keywords={topics} description={excerpt.excerpt} lang="en" meta={[]} />
       <main className="bg-quarternary text-primary min-h-screen py-8 px-12">
         <Header />
         <div className="blogpost flex flex-col py-8 md:w-2/3 lg:w-3/5 xl:w-2/4 m-auto">
-          <h2 className="text-xl md:text-3xl text-center font-bold mb-8">{title}</h2>
+          <h2 className="text-2xl md:text-4xl text-center font-bold mb-8">{title}</h2>
           <img src={banner.file.url} alt="banner" className="w-full md:w-3/5 mb-16 m-auto" />
-          <div className="mb-4 py-4 border-t border-b border-gray-300">
-            <p>published: {renderDate(createdAt)} </p>
-            <p>last update: {renderDate(updatedAt)}</p>
-            <p>category: {renderTopics(topics)}</p>
+          <div className="mb-4 py-4 border-t border-b border-gray-300 text-base md:text-lg">
+            <p className="mb-2 flex items-start">
+              <span className="bg-primary text-quarternary px-2 rounded-md">published:</span>
+              <span className="ml-2">{renderDate(createdAt)}</span>
+            </p>
+            <p className="mb-2 flex items-start">
+              <span className="bg-primary text-quarternary px-2 rounded-md">last update:</span>
+              <span className="ml-2">{renderDate(updatedAt)}</span>
+            </p>
+            <p className="flex items-start">
+              <span className="bg-primary text-quarternary px-2 rounded-md">category:</span>
+              <span className="ml-2">{renderTopics(topics)}</span>
+            </p>
           </div>
-          <div className="leading-relaxed">{renderRichText(content.json)}</div>
+          <div className="leading-relaxed text-base md:text-lg">{renderRichText(content.json)}</div>
         </div>
       </main>
       <Footer />
@@ -49,6 +58,9 @@ export const pageQuery = graphql`
       }
       content {
         json
+      }
+      excerpt {
+        excerpt
       }
     }
   }
