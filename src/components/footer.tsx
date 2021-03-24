3b0c4e83a6c02076dd0ca0192a1e781621769613
iwgx <wahabiputra@gmail.com>
2021-03-25 00:46:20 +0700

    Use react-icons instead

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReddit, faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FaTwitter, FaLinkedin, FaReddit, FaGithub } from 'react-icons/fa';
 
 const icons = [
-  { icon: faTwitter, label: 'twitter', url: 'https://twitter.com/ilhamwahabigx' },
-  { icon: faLinkedin, label: 'linkedin', url: 'https://www.linkedin.com/in/ilham-wahabi' },
-  { icon: faReddit, label: 'reddit', url: 'https://www.reddit.com/user/ilhamwahabi' },
-  { icon: faGithub, label: 'github', url: 'https://github.com/iwgx' },
+  { Icon: FaTwitter, label: 'twitter', url: 'https://twitter.com/ilhamwahabigx' },
+  { Icon: FaLinkedin, label: 'linkedin', url: 'https://www.linkedin.com/in/ilham-wahabi' },
+  { Icon: FaReddit, label: 'reddit', url: 'https://www.reddit.com/user/ilhamwahabi' },
+  { Icon: FaGithub, label: 'github', url: 'https://github.com/iwgx' },
 ];
 
 export const Footer = () => {
@@ -15,9 +14,9 @@ export const Footer = () => {
   return (
     <footer className="bg-quarternary p-12 border-t border-gray-300 flex flex-col items-center">
       <div className="w-full md:w-1/3 lg:w-1/6 flex justify-around">
-        {icons.map(({ icon, label, url }, index) => (
+        {icons.map(({ Icon, label, url }, index) => (
           <a href={url} target="_blank" rel="noopener noreferrer" key={index} aria-label={label}>
-            <FontAwesomeIcon className="text-xl" icon={icon} />
+            <Icon className="text-xl" />
           </a>
         ))}
       </div>
