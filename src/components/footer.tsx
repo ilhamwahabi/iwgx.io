af5f43a30bd7a9a8c7b56bbfb195db0f0040d236
iwgx <wahabiputra@gmail.com>
2021-03-25 11:47:15 +0700

    Add portfolio link to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,6 +14,16 @@ export const Footer = () => {
   return (
     <footer className="bg-quarternary p-12 border-t border-gray-300 flex flex-col items-center">
       <div className="w-full md:w-1/3 lg:w-1/6 flex justify-around">
+        <a
+          className="text-lg font-bold pb-0 border-b border-black leading-6"
+          href="https://portfolio.iwgx.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Portfolio
+        </a>
+      </div>
+      <div className="w-full md:w-1/3 lg:w-1/6 flex justify-around mt-12">
         {icons.map(({ Icon, label, url }, index) => (
           <a href={url} target="_blank" rel="noopener noreferrer" key={index} aria-label={label}>
             <Icon className="text-xl" />
